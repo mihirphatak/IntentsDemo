3c2b863b0817b0cf57ae2caf40d6b4f5051b2e75
Mihir Phatak <32159532+mihirphatak@users.noreply.github.com>
2023-09-05 18:43:24 -0400

    Added movemeent for cancel button.

--- app/src/main/java/com/example/intentsdemo/SelectMood.java
+++ app/src/main/java/com/example/intentsdemo/SelectMood.java
@@ -3,6 +3,7 @@ package com.example.intentsdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.SeekBar;
 
 public class SelectMood extends AppCompatActivity {
@@ -14,5 +15,19 @@ public class SelectMood extends AppCompatActivity {
         setContentView(R.layout.activity_select_mood);
 
         setTitle("Set Mood");
+
+        findViewById(R.id.buttonMoodCancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        findViewById(R.id.buttonMoodSubmit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
