3ee1fd3c086f682f13f48f6437c60fa324b5746b
Mihir Phatak <32159532+mihirphatak@users.noreply.github.com>
2023-09-05 18:37:37 -0400

    Can move from main page to the select mood activity but back is not implemented yet.

--- app/src/main/java/com/example/intentsdemo/MainActivity.java
+++ app/src/main/java/com/example/intentsdemo/MainActivity.java
@@ -1,10 +1,36 @@
 package com.example.intentsdemo;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText name, age;
+    ImageView moodImage;
+
+    private ActivityResultLauncher<Intent> startMoodForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                    } else {
+
+                    }
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +38,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setTitle("Mood");
+
+        moodImage = (ImageView) findViewById(R.id.imageViewMoodView);
+        moodImage.setImageDrawable(null);
+
+        name = findViewById(R.id.editTextName);
+        age = findViewById(R.id.editTextAge);
+
+        findViewById(R.id.buttonSelectMood).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SelectMood.class);
+                startMoodForResult.launch(intent);
+            }
+        });
+
+        findViewById(R.id.buttonSubmit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/intentsdemo/SelectMood.java
+++ app/src/main/java/com/example/intentsdemo/SelectMood.java
@@ -3,9 +3,11 @@ package com.example.intentsdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class SelectMood extends AppCompatActivity {
 
+    SeekBar mood;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
